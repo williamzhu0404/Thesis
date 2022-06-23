545af4740b1a2c6603e53349856dc6a11a992ae2
williamzhu0404 <william.zhu0404@gmail.com>
2022-06-22 23:46:19 -0400

    Now R script produces RData file in the same directory.

--- recall_in_taiwan/data/clean_legislative_api.R
+++ recall_in_taiwan/data/clean_legislative_api.R
@@ -1,12 +1,16 @@
 library(dplyr)
 library(tidyr)
+
+library(this.path)
+
 library(readr)
 
-bill_proposal <- read_csv("Thesis/recall_in_taiwan/data/bill_proposal.csv", 
+setwd(here())
+
+bill_proposal <- read_csv("bill_proposal.csv", 
                           col_types = cols(date = col_integer(), 
                                            term = col_integer(), sessionPeriod = col_integer(), 
                                            sessionTimes = col_integer()))
-
 ROC_0 = 1911
 
 cleaned_df <- bill_proposal %>% mutate(ce_date =  
@@ -15,5 +19,6 @@ cleaned_df <- bill_proposal %>% mutate(ce_date =
                                              as.integer(date +ROC_0 * 10000)
                                            ), format = "%Y%m%d"
                                          ), .before = date)
-saveRDS(cleaned_df, file = "bill_proposal")
+saveRDS(cleaned_df, file = "bill_proposal.RData")
 View(cleaned_df)
+cur_dir <- rstudioapi::getSourceEditorContext()
